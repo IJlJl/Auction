70fabd5f7e207a011aaa3748931795e709bd904a
Ilya <ilyaandrychuk@gmail.com>
2026-03-21 23:20:52 +0200

    Update: Added auctions logic and entity to service

--- Backend/src/auctions/auctions.service.ts
+++ Backend/src/auctions/auctions.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Auction } from './entities/auction.entity';
 import { CreateAuctionDto } from './dto/create-auction.dto';
 import { UpdateAuctionDto } from './dto/update-auction.dto';
 
 @Injectable()
 export class AuctionsService {
-  create(createAuctionDto: CreateAuctionDto) {
-    return 'This action adds a new auction';
+  constructor(
+    @InjectRepository(Auction)
+    private readonly auctionRepository: Repository<Auction>,
+  ) {}
+
+ 
+  async create(createAuctionDto: CreateAuctionDto) { 
+    const auction = this.auctionRepository.create(createAuctionDto);
+    return await this.auctionRepository.save(auction);
   }
 
-  findAll() {
-    return `This action returns all auctions`;
+ 
+  async findAll() {
+    return await this.auctionRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auction`;
+  async findOne(id: string) { 
+    const auction = await this.auctionRepository.findOneBy({ id });
+    if (!auction) {
+      throw new NotFoundException(`Аукціон з ID ${id} не знайдено`);
+    }
+    return auction;
   }
 
-  update(id: number, updateAuctionDto: UpdateAuctionDto) {
-    return `This action updates a #${id} auction`;
+  
+  async update(id: string, updateAuctionDto: UpdateAuctionDto) {
+    const auction = await this.findOne(id);
+    const updated = Object.assign(auction, updateAuctionDto); 
+    return await this.auctionRepository.save(updated);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auction`;
+  
+  async remove(id: string) {
+    const auction = await this.findOne(id);
+    return await this.auctionRepository.remove(auction);
   }
 }
