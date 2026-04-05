93c089d3cefa9a9135c84d258a51c6f5c7d576d1
IJlJl <ilyaandrychuk@gmail.com>
2026-04-05 23:36:52 +0300

    Update auth.module.ts

--- Backend/src/auth/auth.module.ts
+++ Backend/src/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtModuleOptions } from '@nestjs/jwt'; // Додай JwtModuleOptions
+import { JwtModule, JwtModuleOptions } from '@nestjs/jwt'; 
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
